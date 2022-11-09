f6656d8b10cd15dab94771f3f7faeb6a57a40c3c
Samir2100 <samir.timory@tekschool.us>
2022-11-09 17:58:24 -0500

    First commit

---
+++ src/gitTest/GitTestClass.java
@@ -0,0 +1,13 @@
+package gitTest;
+
+public class GitTestClass {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		int y = 5;
+		System.out.println(x + y);
+		System.out.println("This is from local Repo");
+
+	}
+}
